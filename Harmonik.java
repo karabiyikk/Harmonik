4646f0b72aaed2e62fc4726f4f4943193f96c84b
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-18 00:49:51 +0300

    Update Harmonik.java

--- Harmonik.java
+++ Harmonik.java
@@ -1,5 +1,3 @@
-package Donguler;
-
 import java.util.Scanner;
 
 public class Harmonik {
@@ -8,7 +6,7 @@ public class Harmonik {
         Scanner input = new Scanner(System.in);
         System.out.print("'n' sayısını giriniz : ");
         n = input.nextInt();
-        double i, toplam = 0;/// java i'ye int deseydik 1/2yi 1 olarak alır çünkü java int değeri böldüğünde sonucu int alır.
+        double i, toplam = 0;
         for (i = 1; i <= n; i++) {
             toplam += (1 / i);
         }
